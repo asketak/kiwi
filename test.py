75aafb1a6fe86252ace6f269190375f30bf692cf
asketak <simatomas@seznam.cz>
2016-07-28 10:03:49 +0200

    bug in tests

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from .kiwi import *
 
 def test():
 	with open('data.txt', 'r') as myfile:
-		data=myfile.read()
 		output=SearchFlight(data).compute()
 		assert "PV511,PV967,PV731" in output
 		assert "PV511,PV967" in output
