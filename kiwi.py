f815df37a9d4ced8db1b9041929b2275a848d241
asketak <simatomas@seznam.cz>
2016-07-29 00:26:22 +0200

    all seems working, need just duplicates check and add test for it?

--- kiwi.py
+++ kiwi.py
@@ -1,10 +1,9 @@
-import csv,sys
+import csv,sys,time
 from pprint import pprint
 
 def diffdates(d1, d2):
-    #Date format: %Y-%m-%d %H:%M:%S
-    return (time.mktime(time.strptime(d2,"%Y-%m-%d %H:%M:%S")) -
-               time.mktime(time.strptime(d1, "%Y-%m-%d %H:%M:%S")))
+    return (time.mktime(time.strptime(d2,"%Y-%m-%dT%H:%M:%S")) -
+               time.mktime(time.strptime(d1, "%Y-%m-%dT%H:%M:%S")))
 
 class Search_flights(object):
 	"""Main class for searching in flights"""
@@ -19,37 +18,31 @@ class Search_flights(object):
 		for row in reader:
 			for h, v in zip(headers, row):
 				self.column[h].append(v)
-		pprint(self.column)
-		print "====================="
 
 	def next_node(self,previous_flight,hist):
 		""" Returns all flights, that can be attented"""
 		ret = []
 
-		if len(hist) > 5:
-			return ret
 		for x in xrange(0,len(self.column["source"])):
-			if (self.column["destination"][previous_flight] == self.column["source"][x]):
-
-	 #and self.column["arrival"][previous_flight] <= self.column["departure"][x]) :
+			if (self.column["destination"][previous_flight] == self.column["source"][x]
+			and diffdates(self.column["arrival"][previous_flight], self.column["departure"][x]) < 3600*4  
+			and diffdates(self.column["arrival"][previous_flight], self.column["departure"][x]) > 3600)  :
 				ret.append(x);
-		print "ret-next node"
-		pprint(ret)
 		return ret
 
 		
 	def bfs(self, previous_flight, history):
 		"""recursive BFS with checking for time and duplicates"""
-		print("called with ", previous_flight , history)
 		history.append(previous_flight)
 		nodes = self.next_node(previous_flight,history)
 		if len(nodes) > 0:
 			for nxt in nodes:
 				self.bfs(nxt,history[:])
+		if len(history)>1:
+			print(",".join([ self.column["flight_number"][x] for x in history]))
 
 	def compute(self):
-		for x in xrange(1,len(self.column["source"])):
-			print "incompute"
+		for x in xrange(0,len(self.column["source"])):
 			self.bfs(x,[])
 
 with open('data.txt', 'r') as myfile:
