138b45d7dac8199017d86432803b4a3028efb1e1
asketak <simatomas@seznam.cz>
2016-07-28 12:51:02 +0200

    fixing typo and adding test

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ from .kiwi import *
 
 def test():
 	with open('data.txt', 'r') as myfile:
-		output=SearchFlight(data).compute()
+		output=Search_flights(myfile).compute()
 		assert "PV511,PV967,PV731" in output
 		assert "PV511,PV967" in output
 		assert "PV967,PV731" in output
+		assert "PV719,PV493" not in output
