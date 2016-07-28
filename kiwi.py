45a979e8366ec68075db40ee11d871de4da8bad2
asketak <simatomas@seznam.cz>
2016-07-28 10:23:11 +0200

    adding parsing of csv file

--- kiwi.py
+++ kiwi.py
@@ -1,12 +1,35 @@
-class Node:
+import csv,sys
+from pprint import pprint
+
+class Node(object):
+		"""Node for BFS, time and place"""
+		def __init__(self, code, time):
+			self.code = code
+			self.time = time
+				
+class Search_flights(object):
 	"""Time and place of traveler"""
-	def __init__(self, place, time):
-		self.place = place
-		self.time = time
-
-
-def bfs( Node from, Node too,Node[] history):
-	if from.code == too.code:
-
-	for next in next_stop(Node from):
+	def __init__(self,csv_data):
+		reader = csv.reader(csv_data)
+		headers = reader.next()
+		column = {h:[] for h in headers}
+		for row in reader:
+			for h, v in zip(headers, row):
+				column[h].append(v)
+		pprint(column)    # Pretty printer
+		
+	def compute():
 		pass
+
+	def bfs( start, history):
+		"""recursive BFS with checking for time and duplicates"""
+		for nxt in next_node(start):
+			break
+
+with open('data.txt', 'r') as myfile:
+	s = Search_flights(myfile)
+#	s.compute()
+
+
+#Search_flights(sys.stdin.read()).compute()
+
