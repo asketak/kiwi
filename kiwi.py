90916bf10a94fc1b8bfb90feed471984477f95ab
asketak <simatomas@seznam.cz>
2016-07-28 12:51:44 +0200

    seems like working bfs, need to implement the next node

--- kiwi.py
+++ kiwi.py
@@ -1,34 +1,45 @@
 import csv,sys
 from pprint import pprint
 
-class Node(object):
-		"""Node for BFS, time and place"""
-		def __init__(self, code, time):
-			self.code = code
-			self.time = time
-				
 class Search_flights(object):
-	"""Time and place of traveler"""
+	"""Main class for searching in flights"""
+
+	column = None
+	output = None
+
 	def __init__(self,csv_data):
 		reader = csv.reader(csv_data)
 		headers = reader.next()
-		column = {h:[] for h in headers}
+		self.column = {h:[] for h in headers}
 		for row in reader:
 			for h, v in zip(headers, row):
-				column[h].append(v)
-		pprint(column)    # Pretty printer
-		
-	def compute():
-		pass
+				self.column[h].append(v)
 
-	def bfs( start, history):
+	def next_node(self,previous_flight,hist):
+		if len(hist) > 2:
+			return []
+		return [3,5,2]
+#		for x in xrange(1,len(self.column)):
+
+		
+	def bfs(self, previous_flight, history):
 		"""recursive BFS with checking for time and duplicates"""
-		for nxt in next_node(start):
-			break
+		print("called with ", previous_flight , history)
+		history.append(previous_flight)
+		nodes = self.next_node(previous_flight,history)
+		if len(nodes) > 0:
+			for nxt in nodes:
+				self.bfs(nxt,history[:])
+
+	def compute(self):
+		for x in xrange(1,len(self.column["source"])):
+			print "incompute"
+			history = []
+			self.bfs(x,history)
 
 with open('data.txt', 'r') as myfile:
 	s = Search_flights(myfile)
-#	s.compute()
+	s.compute()
 
 
 #Search_flights(sys.stdin.read()).compute()
