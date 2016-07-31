4806019fb05138cbe069c2a5866bbbd25fedf67a
asketak <simatomas@seznam.cz>
2016-07-31 23:00:34 +0200

    reworking tests

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import unittest
-from .kiwi import *
 
-def test():
-	with open('data.txt', 'r') as myfile:
-		output=Search_flights(myfile).compute()
-		assert "PV511,PV967,PV731" in output
-		assert "PV511,PV967" in output
-		assert "PV967,PV731" in output
-		assert "PV719,PV493" not in output
+def test(capsys):
+	from .kiwi import *
+	output, err = capsys.readouterr()
+	assert "PV511,PV967,PV731" in output
+	assert "PV511,PV967" in output
+	assert "PV967,PV731" in output
+	assert "PV719,PV493" in output
